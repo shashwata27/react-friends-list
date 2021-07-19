aa7031a3cb261aa712dfd99b107e51519e9eb248
Shashwata Saha <sahas277@gmail.com>
2021-07-19 15:34:12 +0530

    lifting state up to parent

--- src/components/People.jsx
+++ src/components/People.jsx
@@ -2,34 +2,33 @@ import React from "react";
 import bussinessIcon from "../icons/team-leader.svg";
 export default class People extends React.Component {
   state = {
-    name: this.props.name,
-    work: this.props.work,
-    city: this.props.city,
-    phone: this.props.phone,
-    email: this.props.email,
     visible: "",
     important: "",
   };
+
   handelDelete = () => {
-    this.setState({ visible: "invisible" });
+    this.props.deleteFunc(this.props.id);
+    // this.setState({ visible: "invisible" });
   };
   handelImportant = () => {
+    this.props.importantFunc(this.props.id);
     this.state.important === ""
       ? this.setState({ important: "important" })
       : this.setState({ important: "" });
   };
 
   render() {
+    const { name, work, city, phone, email } = this.props.details;
     return (
       <div className={`li ${this.state.visible} ${this.state.important}`}>
         <div className="liName">
-          <span>{this.state.name}</span>
+          <span>{name}</span>
         </div>
         <div>
           <img src={bussinessIcon} alt="Bussiness Icon" />
-          {`${this.state.work}, ${this.state.city}`}
+          {`${work}, ${city}`}
         </div>
-        <div className="personalInfo">{`${this.state.phone}     ${this.state.email}`}</div>
+        <div className="personalInfo">{`${phone}     ${email}`}</div>
         <div className="buttons">
           <button onClick={this.handelImportant}>Important</button>
           <button onClick={this.handelDelete}>Delete</button>
--- src/components/PeopleList.jsx
+++ src/components/PeopleList.jsx
@@ -3,16 +3,39 @@ import DATA from "../MOCK_DATA.json";
 import People from "./People";
 
 export default class PeopleList extends React.Component {
+  state = { DATA };
+
+  handleDeleteItem = (id) => {
+    const newState = this.state.DATA.filter((item) => id !== item.id);
+    this.setState({ DATA: newState });
+  };
+
+  handleImportantItem = (id) => {
+    const newState = this.state.DATA.filter((item) => {
+      if (id === item.id) {
+        item.important = true;
+      }
+      return item;
+    });
+    this.setState({ DATA: newState });
+  };
+
   render() {
-    return DATA.map((val) => {
+    return this.state.DATA.map((val) => {
+      const details = {
+        name: val.name,
+        work: val.work,
+        city: val.city,
+        phone: val.phone,
+        email: val.email,
+      };
       return (
         <People
           key={val.id}
-          name={val.name}
-          work={val.work}
-          city={val.city}
-          phone={val.phone}
-          email={val.email}
+          id={val.id}
+          details={details}
+          deleteFunc={this.handleDeleteItem}
+          importantFunc={this.handleImportantItem}
         />
       );
     });
